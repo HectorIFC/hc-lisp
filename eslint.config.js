3a5221d6f97b1983f20c46e0fd221308a2aabf9b
Hector Cardoso <hector.cardoso@grupoboticario.com.br>
2025-08-13 23:39:17 -0300

    feat: build

--- eslint.config.js
+++ eslint.config.js
@@ -88,8 +88,59 @@ module.exports = [
             // Allow undef since TypeScript handles this
             'no-undef': 'off',
             
+            // Disable all comments using plugin (but allow shebangs)
             // Disable all comments using plugin
             'no-comments/disallowComments': 'error'
+        }
+    },
+    // Special rules for CLI executables (allow shebangs)
+    {
+        files: ['**/hc-runner.ts', '**/repl.ts'],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module'
+            },
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                require: 'readonly',
+                module: 'readonly',
+                exports: 'readonly',
+                global: 'readonly'
+            }
+        },
+        plugins: {
+            '@typescript-eslint': typescript
+        },
+        rules: {
+            // All TypeScript rules from main config
+            'no-console': 'off',
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'comma-dangle': ['error', 'never'],
+            'max-len': ['error', { code: 140 }],
+            'no-empty': 'off',
+            'no-shadow': 'off',
+            'no-var': 'error',
+            'prefer-const': 'error',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-this-alias': 'off',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/prefer-as-const': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+            'no-undef': 'off'
+            // Note: No comment restrictions for CLI files (allows shebangs)
         }
     },
     {
